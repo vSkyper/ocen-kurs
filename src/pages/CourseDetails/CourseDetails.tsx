136f6b46ea5d06006c8d8b1ed3528d0a7b4be339
Karol Filipiak <karfil3@st.amu.edu.pl>
2024-06-15 18:50:12 +0200

    Reduced size of Review details and centered content

--- src/pages/CourseDetails/CourseDetails.tsx
+++ src/pages/CourseDetails/CourseDetails.tsx
@@ -2,7 +2,7 @@ import { ReviewList, CourseDescription } from './components';
 
 export default function CourseDetails() {
   return (
-    <div className='m-16'>
+    <div className='m-16 mx-auto w-[1048px]'>
       <CourseDescription />
       <ReviewList />
     </div>

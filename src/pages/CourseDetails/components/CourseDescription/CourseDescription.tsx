25c6bfe95bc07fa4bfa147830a06177d87d116f2
Dominik Jagosz <domjag5@st.amu.edu.pl>
2024-06-15 21:09:23 +0200

    styled CourseDetails

--- src/pages/CourseDetails/components/CourseDescription/CourseDescription.tsx
+++ src/pages/CourseDetails/components/CourseDescription/CourseDescription.tsx
@@ -5,34 +5,36 @@ export default function CourseDescription() {
     <div className='mb-8'>
       <div className='mb-8'>
         <h2 className='mb-2 text-center text-4xl font-semibold'>
-          Podstawy bezpieczeństwa komputerowego
+          Zaawansowane Techniki Marketingowe
         </h2>
         <h3 className='text-center text-xl hover:underline cursor-pointer'>
-          Uniwersytet Warmińsko-Mazurski
+          Marketing Masters
         </h3>
       </div>
       <div className='mb-4 p-4 rounded-3xl bg-zinc-800'>
-        <p className='indent-8 justify-normal break-normal'>
-          To jest opis kursu, który jest opisywany. Opis jest krótki, ale
-          jednocześnie dostatecznie długi. Może mieć wiele linijek. Teraz coś
-          prostego: wcięcia pierwszej linii każdego akapitu. Wiele osób uważa,
-          że wcięcia akapitowe są łatwiejsze do czytania niż odstępy
-          międzyakapitowe (stosowane m.in. w języku angielskim, co przyczyniło
-          się także do domyślnego podziału tekstu dla HTML-a i tagu P),
-          szczególnie w dłuższych tekstach.
+        <p className='indent-8 text-justify break-normal'>
+          Kurs dla profesjonalistów z dziedziny marketingu. To jest nieco
+          obszerniejszy opis kursu, który jest opisywany. Opis jest krótki, ale
+          jednocześnie dostatecznie długi. Może mieć wiele linijek. Kurs dotyczy
+          marketingu i tego właśnie można się nauczyć. Zajęcia są prowadzone w
+          formule najlepiej dopasowanej do przekazywanych treści. Ukończenie
+          kursu jest zakończone egzaminem certyfikującym, który pozwala otrzymać
+          certyfikat ukończenia kursu i zaliczenia egzminu. Program kursu został
+          starannie przygotowany przez zespół układający program kursu. Opis
+          kursu pochodzi ze strony organizatora.
         </p>
       </div>
       <div className='mb-4 overflow-clip rounded-3xl bg-zinc-800'>
-        <table className='table-fixed rounded-3xl bg-zinc-800 w-full'>
-          <tbody className='rounded-3xl bg-zinc-800'>
+        <table className='table-fixed w-full'>
+          <tbody className=''>
             <tr>
               <td className='text-lg font-semibold'>Strona kursu:</td>
               <td className=''>
                 <a
                   className='underline text-sky-400 visited:text-violet-400'
-                  href='https://www.w3schools.com'
+                  href='https://www.marketingmasters.com/zaawansowane_techniki_marketingowe'
                 >
-                  https://www.w3schools.com
+                  www.marketingmasters.com/ztm
                 </a>
               </td>
             </tr>
@@ -42,17 +44,33 @@ export default function CourseDescription() {
             </tr>
             <tr>
               <td className='text-lg font-semibold'>Cena:</td>
-              <td>darmowy</td>
+              <td>499.99 PLN</td>
+            </tr>
+            <tr>
+              <td className='text-lg font-semibold'>Język kursu:</td>
+              <td>angielski</td>
             </tr>
             <tr>
               <td className='text-lg font-semibold'>Średnia ocen:</td>
-              <td>4,8</td>
+              <td>4.9</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div className='flex flex-auto gap-16 items-start'>
-        <Chip className='mb-0' variant='outlined' label='kategoria' />
+      <div className='flex flex-auto mx-5 gap-4 items-start'>
+        <p>Tagi:</p>
+        <Chip
+          className='mb-0'
+          size='medium'
+          variant='outlined'
+          label='Marketing'
+        />
+        <Chip
+          className='mb-0'
+          size='medium'
+          variant='outlined'
+          label='Średniozaawansowany'
+        />
       </div>
     </div>
   );

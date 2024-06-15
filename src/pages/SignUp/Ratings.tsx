1d7d835ad072b41c5097b5c17cf6910022579263
Karol Filipiak <karfil3@st.amu.edu.pl>
2024-06-15 16:50:46 +0200

    Merge conflict resolve + code cleanup

--- src/pages/SignUp/Ratings.tsx
+++
@@ -1,3 +0,0 @@
-export default function Ratings() {
-  return <div>Ratings</div>;
-}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import Ratings from './Ratings';
-
-export { Ratings };
-=======
-import Ratings from './SignUp';
 import CourseDetails from './CourseDetails';
 
-export { Ratings, CourseDetails };
->>>>>>> 470c16ca10284e06921b0ccbe25029406ac1a444
+export { CourseDetails };

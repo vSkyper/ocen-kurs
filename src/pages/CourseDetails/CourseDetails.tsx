7284d77efe01dbf38ce8836eb5846fc858a4fd27
Mateusz Piwowarski <skyperpl@gmail.com>
2024-06-15 21:11:37 +0200

    ScrollToTop

--- src/pages/CourseDetails/CourseDetails.tsx
+++ src/pages/CourseDetails/CourseDetails.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { ReviewList, CourseDescription } from './components';
 
 export default function CourseDetails() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className='m-16 mx-auto w-[1048px]'>
       <CourseDescription />
